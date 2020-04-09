43c19418c6d289ccd037abab4868c545e9965b06
Kelly <k.tompkins005@gmail.com>
2020-04-08 22:18:28 -0500

    redoing sober calculator with python script first

---
+++ app/calendar_date.py
@@ -0,0 +1,13 @@
+import datetime
+
+def sober_date():
+    date_entry = input('Enter a date in YYYY-MM-DD format: ')
+    year, month, day = map(int, date_entry.split('-'))
+    date1 = datetime.date(year, month, day)
+
+    today = datetime.date.today()
+    diff = (today - date1).days
+    print("You have stayed sober for {} days! Good job!!!".format(diff))
+
+
+sober_date()
