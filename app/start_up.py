e4b7325fb34d1b476cdc37ee27ece775021b37b7
Kelly <k.tompkins005@gmail.com>
2020-04-10 10:15:28 -0500

    adding configuration for webform

--- app/start_up.py
+++ app/start_up.py
@@ -1,7 +1,10 @@
-from flask import Flask
-import datetime
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitFielld
 
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 @app.route('/')
 def index():
