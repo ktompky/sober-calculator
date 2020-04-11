a4ea2ed018a394c98c1d81ec3fccce80668a7bb1
Kelly <k.tompkins005@gmail.com>
2020-04-11 17:31:18 -0500

    moved main app and config to fix module issue

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, flash, request
+from config import Config
+
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+
+@app.route('/')
+
+@app.route('/calculator')
+def calculator():
+    return render_template('hello.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign in", form=form)
