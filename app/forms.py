7cad90ea4f9dc61b90cd4c4ec58c2168a1ae3e96
Kelly <k.tompkins005@gmail.com>
2020-04-12 21:09:33 -0500

    adding username and sobriety date

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class DateForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    date    = IntegerField('Enter a date in YYYY-MM-DD format: ', validators= [DataRequired()])
+    date    = StringField('Enter a date in YYYY-MM-DD format: ', validators= [DataRequired()])
     submit = SubmitField('Get Date')
