0e5c5cc91a9b8ec3675f73e28edcdad8237419af
Kelly <k.tompkins005@gmail.com>
2020-04-13 19:47:51 -0500

    adding procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath app sobriety_app:app
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345catmeowmew'
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
