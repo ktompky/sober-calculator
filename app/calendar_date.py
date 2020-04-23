30ab85d758cd83b8ec430814aea64563c4966785
Kelly <k.tompkins005@gmail.com>
2020-04-23 15:28:26 -0500

    adding calendar to date picker

--- app/calendar_date.py
+++ app/calendar_date.py
@@ -1,13 +1,7 @@
+from app import app
+from flask import render_template,flash, redirect, request
+from app.forms import DateForm
 import datetime
 
-def sober_date():
-    date_entry = input('Enter a date in YYYY-MM-DD format: ')
-    year, month, day = map(int, date_entry.split('-'))
-    date1 = datetime.date(year, month, day)
-
-    today = datetime.date.today()
-    diff = (today - date1).days
-    return "You have stayed sober for {} days! Good job!!!".format(diff)
 
 
-sober_date()
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm,RecaptchaField
-from wtforms import StringField, PasswordField, IntegerField, SubmitField,DateField,SelectField
+from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import InputRequired,DataRequired
+from wtforms.fields.html5 import DateField
 
 class DateForm(FlaskForm):
-    username = StringField('Username', [InputRequired()])
-    date    = DateField('Enter a date in YYYY-MM-DD format: ', [DataRequired()], format='%Y-%d-%m' )
+    username = StringField('Enter your first name: ', [InputRequired()])
+    date    = DateField('Enter you first date of sobriety: ', [DataRequired()], format='%Y-%d-%m' )
     submit = SubmitField('Get Date')
 
+ 
